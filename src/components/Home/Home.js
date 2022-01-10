2bbe729bbde64c604b3396a75f8647cb47d8b3bf
Marian Busila <mbusila@pfdrive.com>
2022-01-09 22:26:50 -0500

    call api to get movies

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../common/apis/MovieApi";
+import { APIKey } from "../../common/apis/MovieApiKey";
 
 export default function Home() {
-  return <div>Home</div>;
+  useEffect(() => {
+    const movieText = "Harry";
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => console.log(err));
+      console.log(response);
+    };
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
+  );
 }
