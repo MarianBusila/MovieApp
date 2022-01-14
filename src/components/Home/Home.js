3a78ec9256c33eee6fc7985e89e65ea9d3f71821
Marian Busila <mbusila@pfdrive.com>
2022-01-14 13:02:49 -0500

    add redux store and movie slice and display movies

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,15 +2,19 @@ import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../common/apis/MovieApi";
 import { APIKey } from "../../common/apis/MovieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 export default function Home() {
+  const movieText = "Harry";
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    const movieText = "Harry";
     const fetchMovies = async () => {
       const response = await movieApi
         .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
         .catch((err) => console.log(err));
-      console.log(response);
+      dispatch(addMovies(response.data));
     };
     fetchMovies();
   }, []);
